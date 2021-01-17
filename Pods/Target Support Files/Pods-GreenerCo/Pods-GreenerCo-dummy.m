38018478da31253295b5ff9cec4f91a1e21c0a99
Nikita Oltyan <nikitaoltyan@mail.ru>
2021-01-17 09:09:05 +0300

    1

---
+++ Pods/Target Support Files/Pods-GreenerCo/Pods-GreenerCo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GreenerCo : NSObject
+@end
+@implementation PodsDummy_Pods_GreenerCo
+@end
---
+++ Pods/Target Support Files/Pods-GreenerCoTests/Pods-GreenerCoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GreenerCoTests : NSObject
+@end
+@implementation PodsDummy_Pods_GreenerCoTests
+@end
